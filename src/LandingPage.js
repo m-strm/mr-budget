c39e094c73603e98524641e2f47a8047d8d15845
Micah Sturm <Sturm.mx@gmail.com>
2019-07-31 20:58:02 -0500

    Landing Page Added

---
+++ src/LandingPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+
+const LandingPage = () => (
+    <header>
+      <h1>Mr.Budget is under construction</h1>
+      <h2>Come back soon!</h2>
+    </header>
+  );
+
+ReactDOM.render(<LandingPage />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 console.log(__dirname);
 
 module.exports = {
-  entry: "./src/app.js",
+  entry: "./src/LandingPage.js",
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js"
